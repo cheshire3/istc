f25192115ab33da37dbddefb30e991f40ac52389
John Harrison <john.harrison@liv.ac.uk>
2013-06-19 11:14:08 +0100

    update German script from live version

--- dbs/istc/dataCleaning/changeGerman.py
+++ dbs/istc/dataCleaning/changeGerman.py
@@ -23,80 +23,132 @@ parser = db.get_object(session, 'LxmlParser')
 #Set this to true if you want to keep private data associated with the record false if you want to replace all the data
 keepPrivate = True 
 
-dir = '/home/cheshire/cheshire3/dbs/istc/data/'
-
-          
+datadir = '/home/cheshire/cheshire3/dbs/istc/data'
+outdir = '/home/cheshire/cheshire3/dbs/istc/dataTemp'
+         
             
 #add the new ones from the datafile
 newData = '/home/cheshire/cheshire3/dbs/istc/dataCleaning/newGermanData.txt'
 
-file = codecs.open( newData, "r", "utf-8" )
-lines = file.readlines()
-file.close()
 missing = []
 
 print 'changing German data'
-for i, l in enumerate(lines):
-
-    if (len(l.strip()) > 0 and l.strip().find('#RA#') == 0):
-        number = l.strip()[l.find('i')-1:].replace('#', '')
-        data = lines[i+1].strip()[10:].strip().replace('#', '')
-        dataList = data.split(';')
-        try:
-            targetFile = open(dir + number + '.xml', 'r')
-        except:
-            missing.append(number)
-        else:
-            targetTree = etree.fromstring(targetFile.read())
-            targetFile.close()
-            
-            parent = targetTree.xpath('/record')[0]        
-            
-            #remove current 997 fields in record (including or not the private data depending on flag)
-            germanlocs = targetTree.xpath('//datafield[@tag="997"]')
-            
-            if keepPrivate == True:
-                for entry in germanlocs:
-                    if not entry.xpath('./subfield[@code="x"]'):
-                        parent.remove(entry)
+if "--location-per-line" in sys.argv:
+    fh = codecs.open('/home/cheshire/cheshire3/dbs/istc/dataCleaning/Germanlocs2010.TXT', "r", "utf-8" )
+    lines = fh.readlines()
+    fh.close()
+    for l in lines:
+        l = l.strip()
+        if not l:
+            continue
+        elif l.find('#RA#') == 0:
+            number = l[l.find('i')-1:].replace('#', '')
+            try:
+                targetFile = open(os.path.join(datadir, number + '.xml'), 'r')
+            except IOError:
+                missing.append(number)
+                number = None
+                targetTree = None
             else:
-                for entry in germanlocs:
-                    parent.remove(entry)
-            
-            
-            for d in dataList:
-                d = d.strip()
-
-                #create new 997 node
-                datafield = etree.Element('datafield', tag='997', ind1='0', ind2='0')
-                
-                if d.find(' (') == -1:
-                    subfieldA = etree.Element('subfield', code='a')   
-                    subfieldA.text = d.replace('*', '')
-                    datafield.append(subfieldA) 
+                targetTree = etree.parse(targetFile)
+    	        targetFile.close()
+    	        parent = targetTree.getroot()
+                #remove current 997 fields in record (including or not the private data depending on flag)
+                germanlocs = targetTree.xpath('//datafield[@tag="997"]')
+                if keepPrivate == True:
+                    for entry in germanlocs:
+                        if not entry.xpath('./subfield[@code="x"]'):
+                            parent.remove(entry)
                 else:
-                    subfieldA = etree.Element('subfield', code='a')   
-                    subfieldA.text = d[:d.find(' (')].replace('*', '')
-                    datafield.append(subfieldA)
-                    
-                    subfieldB = etree.Element('subfield', code='b')   
-                    subfieldB.text = d[d.find(' (')+1:].replace('*', '')
-                    datafield.append(subfieldB)
-                if d.find('*') != -1:
-                    subfieldX = etree.Element('subfield', code='x')
-                    subfieldX.text = 'Private'
-                    datafield.append(subfieldX)
-                    
-                parent.append(datafield)
-                doc = StringDocument(etree.tostring(targetTree))
-                rec = parser.process_document(session, doc)
-                doc2 = indentingTxr.process_record(session, rec)     
-                output = open(cheshirePath + '/cheshire3/dbs/istc/data/' + number + '.xml', 'w')
-                output.write(doc2.get_raw(session))
-                output.flush()
-                output.close()
-    
+                    for entry in germanlocs:
+                        parent.remove(entry)
+            # move on to data line(s)
+            continue
+        try:
+            parent = targetTree.getroot()
+        except (NameError, AttributeError):
+            continue
+        #create new 997 node
+        datafield = etree.Element('datafield', tag='997', ind1='0', ind2='0')
+        data = l[10:].strip(' $#')
+        fieldsList = data.split('$')
+	    # for each MARC sub-field
+        for field in fieldsList:
+            sfcode, field = field[0], field[1:]
+            subfield = etree.Element('subfield', code=sfcode)
+            subfield.text = field.replace('*', '')
+            datafield.append(subfield)
+        parent.append(datafield)
+        doc = StringDocument(etree.tostring(targetTree))
+        rec = parser.process_document(session, doc)
+        doc2 = indentingTxr.process_record(session, rec)     
+        output = open(os.path.join(outdir, number + '.xml'), 'w')
+        output.write(doc2.get_raw(session))
+        output.flush()
+        output.close()
+        
+else:
+    file = codecs.open( newData, "r", "utf-8" )
+    lines = file.readlines()
+    file.close()
 
-              
-print 'MISSING FROM GERMAN'   
-print '\n'.join(missing)
+    for i, l in enumerate(lines):
+        if (len(l.strip()) > 0 and l.strip().find('#RA#') == 0):
+            number = l.strip()[l.find('i')-1:].replace('#', '')
+            data = lines[i+1].strip()[10:].strip().replace('#', '')
+            dataList = data.split(';')
+            try:
+                targetFile = open(os.path.join(datadir, number + '.xml'), 'r')
+            except:
+                missing.append(number)
+            else:
+                targetTree = etree.fromstring(targetFile.read())
+                targetFile.close()
+                parent = targetTree.xpath('/record')[0]        
+                #remove current 997 fields in record (including or not the private data depending on flag)
+                germanlocs = targetTree.xpath('//datafield[@tag="997"]')
+                if keepPrivate == True:
+                    for entry in germanlocs:
+                        if not entry.xpath('./subfield[@code="x"]'):
+                            parent.remove(entry)
+                else:
+                    for entry in germanlocs:
+                        parent.remove(entry)
+                for d in dataList:
+                    d = d.strip()
+                    #create new 997 node
+                    datafield = etree.Element('datafield', tag='997', ind1='0', ind2='0')
+                    if d.find(' (') == -1:
+                        subfieldA = etree.Element('subfield', code='a')   
+                        subfieldA.text = d.replace('*', '')
+                        datafield.append(subfieldA) 
+                    else:
+                        subfieldA = etree.Element('subfield', code='a')   
+                        subfieldA.text = d[:d.find(' (')].replace('*', '')
+                        datafield.append(subfieldA)
+                        
+                        subfieldB = etree.Element('subfield', code='b')   
+                        subfieldB.text = d[d.find(' (')+1:].replace('*', '')
+                        datafield.append(subfieldB)
+                    if d.find('*') != -1:
+                        subfieldX = etree.Element('subfield', code='x')
+                        subfieldX.text = 'Private'
+                        datafield.append(subfieldX)
+                        
+                    parent.append(datafield)
+                    doc = StringDocument(etree.tostring(targetTree))
+                    rec = parser.process_document(session, doc)
+                    doc2 = indentingTxr.process_record(session, rec)     
+                    output = open(os.path.join(outdir, number + '.xml'), 'w')
+                    output.write(doc2.get_raw(session))
+                    output.flush()
+                    output.close()
+    	
+#print 'MISSING FROM GERMAN'
+#print '\n'.join(missing)
+missingFile = open('missingITSCs.txt', 'w')
+for l in missing:
+	missingFile.write(l + '\n')
+	missingFile.flush()
+missingFile.close()
+
