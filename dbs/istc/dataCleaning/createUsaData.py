101664d213073d350300ab4685a14a8681df3af4
John Harrison <john.harrison@liv.ac.uk>
2014-04-23 11:08:40 +0100

    updated createUsaData.py for provided data format

--- dbs/istc/dataCleaning/createUsaData.py
+++ dbs/istc/dataCleaning/createUsaData.py
@@ -1,30 +1,54 @@
+"""Create XML version of USA references data.
 
-#Used to create new USA location data when moving from C2 to C3 (June 2009)
+Originally used to create new USA location data when moving from
+Cheshire 2 to Cheshire3 (June 2009).
+"""
 
+import sys
 
 from lxml import etree
 
-datafile = "newusadata.csv"
-outputfile = "../usaData/usaCodes.xml"
+from istcArguments import FileArgumentParser
+
+# Init OptionParser
+docbits = __doc__.split('\n\n')
+arg_parser = FileArgumentParser(conflict_handler='resolve',
+                                description=docbits[0]
+                                )
+arg_parser.set_defaults(infile="usaData.txt",
+                        outfile="../usaData/usaCodes.xml")
 
 
-file = open(datafile, 'r')
-lines = file.readlines()
-file.close()
-output = []
+def main(argv=None):
+    if argv is not None:
+        args = arg_parser.parse_args(argv)
+    else:
+        args = arg_parser.parse_args()
 
-for l in lines:
-    toks = l.split('\t')
-    output.append('<record><code>%s</code><full>%s, %s</full></record>' % (toks[0], toks[2].replace('&', '&amp;').strip(), toks[1].replace('&', '&amp;').strip()))
+    output = []
 
-tree = etree.fromstring('<usa>%s</usa>' % ''.join(output))           
-parsedXslt = etree.parse("../xsl/reindent.xsl")
-txr = etree.XSLT(parsedXslt)
+    for l in args.infile:
+        try:
+            code, location = l.strip().split('\t')
+        except ValueError:
+            continue
+        output.append('<record><code>{0}</code><full>{1}</full></record>'
+                      ''.format(code,
+                                location.replace('&', '&amp;').strip()
+                                )
+                      )
 
-result = txr(tree)
-resultfile = open(outputfile, 'w')
+    tree = etree.fromstring('<usa>%s</usa>' % ''.join(output))
+    parsedXslt = etree.parse("../xsl/reindent.xsl")
+    txr = etree.XSLT(parsedXslt)
 
-resultfile.write(etree.tostring(result))
-resultfile.flush()
-resultfile.close()
-                  
+    result = txr(tree)
+
+    args.outfile.write(etree.tostring(result))
+    args.outfile.flush()
+    args.outfile.close()
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
